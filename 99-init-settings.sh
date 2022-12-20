a02fcc12b1fc80711c59633a191e1b980c3c183f
davintagas <78451092+davintagas@users.noreply.github.com>
2022-12-20 08:42:36 +0700

    Update 99-init-settings.sh

--- 99-init-settings.sh
+++ 99-init-settings.sh
@@ -7,7 +7,7 @@ uci commit
 
 # add cron job for modem l860-gl
 echo '#auto renew ip lease for modem rakitan' >> /etc/crontabs/root
-echo '0 0,6,18 * * * echo AT+CFUN=1,1 | atinout - /dev/ttyACM2 - && ifdown Fibocom && sleep 3 && ifup Fibocom' >> /etc/crontabs/root
+echo '0 0,6,16 * * * echo AT+CFUN=1,1 | atinout - /dev/ttyACM2 - && ifdown Fibocom && sleep 3 && ifup Fibocom' >> /etc/crontabs/root
 /etc/init.d/cron restart
 
 exit 0
