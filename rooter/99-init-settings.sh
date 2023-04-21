3d5b484818749dab2f012a2c029e6780ec749638
Void-Age <age@void.lan>
2023-04-21 22:56:39 +0700

    renamed:    99-init-settings.sh -> official/99-init-settings.sh 	new file:   rooter/99-init-settings.sh

---
+++ rooter/99-init-settings.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Set Timezone to Asia/Jakarta
+uci set system.@system[0].timezone='WIB-7'
+uci set system.@system[0].zonename='Asia/Jakarta'
+uci commit
+
+# add cron job for modem l860-gl
+echo '#auto renew ip lease for modem rakitan' >> /etc/crontabs/root
+echo '0 0,6,16 * * * echo AT+CFUN=1,1 | atinout - /dev/ttyUSB2 -' >> /etc/crontabs/root
+/etc/init.d/cron restart
+
+exit 0
