0f213683f8c7abff49163ad7ddc395fcca69d7a1
davintagas <78451092+davintagas@users.noreply.github.com>
2024-03-04 08:49:25 +0700

    Update 99-init-settings.sh

--- official/99-init-settings.sh
+++ official/99-init-settings.sh
@@ -6,8 +6,8 @@ uci set system.@system[0].zonename='Asia/Jakarta'
 uci commit
 
 # add cron job for modem l860-gl
-echo '#auto renew ip lease for modem rakitan' >> /etc/crontabs/root
-echo '0 0,6,16 * * * echo AT+CFUN=15 | atinout - /dev/ttyACM2 -' >> /etc/crontabs/root
+echo '# auto renew ip lease for modem rakitan' >> /etc/crontabs/root
+echo '# 0 0,6,16 * * * echo AT+CFUN=15 | atinout - /dev/ttyACM2 -' >> /etc/crontabs/root
 /etc/init.d/cron restart
 
 exit 0
