d3ba82925845ef62536ade8a6c1befd736211c7d
davintagas <78451092+davintagas@users.noreply.github.com>
2024-06-27 10:25:47 +0700

    Update 99-init-settings.sh

--- rooter/99-init-settings.sh
+++ rooter/99-init-settings.sh
@@ -8,6 +8,7 @@ uci commit
 # add cron job for modem l860-gl
 echo '#auto renew ip lease for modem rakitan' >> /etc/crontabs/root
 echo '0 0,6,16 * * * sh -i /usr/lib/rooter/connect/reconnect.sh 1' >> /etc/crontabs/root
+echo '*/30 * * * * sync && echo 3 > /proc/sys/vm/drop_caches' >> /etc/crontabs/root
 /etc/init.d/cron restart
 
 exit 0
