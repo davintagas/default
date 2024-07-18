2f11ec8738ce081aa14269ed0377ce92bf2e3319
davintagas <78451092+davintagas@users.noreply.github.com>
2024-07-18 15:24:03 +0700

    Update kernel-6.6.sh

--- kernel-6.6.sh
+++ kernel-6.6.sh
@@ -5,4 +5,5 @@ cp -r main/target/linux/rockchip/patches-6.6 target/linux/rockchip/
 cp -r main/target/linux/rockchip/armv8/config-6.6 target/linux/rockchip/armv8/
 cp -r main/target/linux/generic/{backport-6.6,hack-6.6,pending-6.6,config-6.6} target/linux/generic/
 cp -r main/include/kernel-6.6 include/
+sed -i 's|KERNEL_PATCHVER:=5.15|KERNEL_PATCHVER:=6.6|g' target/linux/rockchip/Makefile
 rm -rf main
