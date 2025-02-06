0a0268a66ffda9e235f366661bbbf30eb77bdab1
davintagas <78451092+davintagas@users.noreply.github.com>
2025-02-06 08:08:24 +0700

    Update 99-init-settings.sh

--- official/99-init-settings.sh
+++ official/99-init-settings.sh
@@ -5,18 +5,10 @@ uci set system.@system[0].timezone='WIB-7'
 uci set system.@system[0].zonename='Asia/Jakarta'
 uci commit
 
-# add cron job for modem l860-gl
-echo '# Auto Renew IP lease for Modem Rakitan' >> /etc/crontabs/root
-echo '30 3 * * * echo AT+CFUN=15 | atinout - /dev/ttyACM2 -' >> /etc/crontabs/root
-echo '*/30 * * * * sync && echo 3 > /proc/sys/vm/drop_caches' >> /etc/crontabs/root
-/etc/init.d/cron restart
-
-chmod +x /usr/bin/cpustat
-chmod +x /etc/profile.d/30-sysinfo.sh
-sed -i 's|/bin/ash|/bin/bash|g' /etc/passwd
+sed -i '/modemfeed/d' /etc/opkg/distfeeds.conf
 sed -i '/4icg/d' /etc/opkg/distfeeds.conf
 sed -i '/internet/d' /etc/opkg/distfeeds.conf
-sed -i '/modemfeed/d' /etc/opkg/distfeeds.conf
-sed -i '/passwall/d' /etc/opkg/distfeeds.conf
+sed -i '/zapret/d' /etc/opkg/distfeeds.conf
+sed -i '/sms/d' /etc/opkg/distfeeds.conf
 
 exit 0
