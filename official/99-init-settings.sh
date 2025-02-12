f5d456050faefd82339c3ebc9a8af51288e214d4
davintagas <78451092+davintagas@users.noreply.github.com>
2025-02-12 08:07:46 +0700

    Update 99-init-settings.sh

--- official/99-init-settings.sh
+++ official/99-init-settings.sh
@@ -8,7 +8,6 @@ uci commit
 sed -i '/modemfeed/d' /etc/opkg/distfeeds.conf
 sed -i '/4icg/d' /etc/opkg/distfeeds.conf
 sed -i '/internet/d' /etc/opkg/distfeeds.conf
-sed -i '/zapret/d' /etc/opkg/distfeeds.conf
 sed -i '/sms/d' /etc/opkg/distfeeds.conf
 
 exit 0
