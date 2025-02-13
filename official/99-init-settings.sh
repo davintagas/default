2210645f0dff6dbbc2dc9b72e474dd3b0e57e864
davintagas <78451092+davintagas@users.noreply.github.com>
2025-02-13 08:02:50 +0700

    Update 99-init-settings.sh

--- official/99-init-settings.sh
+++ official/99-init-settings.sh
@@ -5,9 +5,4 @@ uci set system.@system[0].timezone='WIB-7'
 uci set system.@system[0].zonename='Asia/Jakarta'
 uci commit
 
-sed -i '/modemfeed/d' /etc/opkg/distfeeds.conf
-sed -i '/4icg/d' /etc/opkg/distfeeds.conf
-sed -i '/internet/d' /etc/opkg/distfeeds.conf
-sed -i '/sms/d' /etc/opkg/distfeeds.conf
-
 exit 0
