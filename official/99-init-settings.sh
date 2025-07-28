21e466774671503e124f988f3859a02e1667d5b9
davintagas <78451092+davintagas@users.noreply.github.com>
2025-07-28 13:11:42 +0700

    Update 99-init-settings.sh

--- official/99-init-settings.sh
+++ official/99-init-settings.sh
@@ -5,6 +5,7 @@ uci set system.@system[0].timezone='WIB-7'
 uci set system.@system[0].zonename='Asia/Jakarta'
 uci commit
 
+sed -i '/l860/d' /etc/opkg/distfeeds.conf
 sed -i 's|ash|bash|g' /etc/passwd
 
 exit 0
