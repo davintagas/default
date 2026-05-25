254bd70c50516c716eec7522d951c25ba7e567a9
davintagas <78451092+davintagas@users.noreply.github.com>
2026-05-25 12:46:44 +0700

    Change distfeeds configuration file for package manager

--- official/99-init-settings.sh
+++ official/99-init-settings.sh
@@ -5,7 +5,7 @@ uci set system.@system[0].timezone='WIB-7'
 uci set system.@system[0].zonename='Asia/Jakarta'
 uci commit
 
-sed -i '/l860/d' /etc/opkg/distfeeds.conf
+sed -i '/l860/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's|ash|bash|g' /etc/passwd
 
 exit 0
